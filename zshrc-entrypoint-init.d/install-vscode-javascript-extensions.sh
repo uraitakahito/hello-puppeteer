ce65a09d24945161c898ad157b59520d3fd3d9ec
Takahito Urai <uraitakahito@gmail.com>
2024-08-16 22:08:49 +0900

    Add vitest.explorer

--- zshrc-entrypoint-init.d/install-vscode-javascript-extensions.sh
+++ zshrc-entrypoint-init.d/install-vscode-javascript-extensions.sh
@@ -7,6 +7,8 @@ extensions=(
   "dbaeumer.vscode-eslint"
   # https://marketplace.visualstudio.com/items?itemName=GitHub.copilot
   "GitHub.copilot"
+  # https://marketplace.visualstudio.com/items?itemName=vitest.explorer
+  "vitest.explorer"
 )
 for extension in ${extensions[@]}; do
   code --install-extension $extension
