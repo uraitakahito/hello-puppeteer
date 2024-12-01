828b8545adcf29452f066f5952d6674a18ce219c
Takahito Urai <uraitakahito@gmail.com>
2024-12-01 15:30:43 +0900

    Add example script for navigation handling with Puppeteer

---
+++ examples/waitForNavigation.mjs
@@ -0,0 +1,28 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto('https://stackoverflow.com/');
+
+    await Promise.all([
+      // https://qiita.com/hnw/items/a07e6b88d95d1656e02f
+      page.waitForNavigation({ waitUntil: ['load', 'networkidle2'] }),
+      page.click('.s-topbar--logo'),
+    ]);
+
+    console.log('Navigated to the login section successfully.');
+
+    // Extracting information from the new page
+    const pageTitle = await page.title();
+    const pageURL = page.url();
+    console.log(`Title: ${pageTitle}`);
+    console.log(`URL: ${pageURL}`);
+
+    await browser.close();
+  } catch (error) {
+    console.error('Error:', error);
+  }
+})();
